2eee891d0216543494726fe890dd56c27dcaa857
Martyn Smith <martyn@dollyfish.net.nz>
2024-12-03 18:10:47 +1300

    day 3

--- day03.py
+++ day03.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python
 
-from functools import reduce
-from rich import print
-from itertools import cycle
+# from rich import print
 from pathlib import Path
-import math
 import re
 
 # https://adventofcode.com/2024/day/03
 
 INPUT_FILENAME = "input03.txt"
-INPUT_FILENAME = "sample03.txt"
+# INPUT_FILENAME = "sample03.txt"
 
-# data = Path(INPUT_FILENAME).read_text()
-data = [line.strip() for line in Path(INPUT_FILENAME).read_text().splitlines()]
+data = Path(INPUT_FILENAME).read_text()
 
 part1 = 0
+for match in re.findall(r"mul\((\d+),(\d+)\)", data):
+    x, y = map(int, match)
+    part1 += x * y
+
 part2 = 0
+part2_active = True
+for is_mul, x, y, is_do, is_dont in re.findall(
+    r"(mul)\((\d+),(\d+)\)|(do)\(\)|(don\'t)\(\)", data
+):
+    if is_do:
+        part2_active = True
+    if is_dont:
+        part2_active = False
+    if is_mul:
+        if part2_active:
+            part2 += int(x) * int(y)
 
-for line in data:
-    print(line)
-
-# print("part1:", part1)
-# print("part2:", part2)
+print("part1:", part1)
+print("part2:", part2)
