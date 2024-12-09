b4cf63462e9cdae03acd0fbb09b7c211f07b0e63
Martyn Smith <martyn@dollyfish.net.nz>
2024-12-09 18:23:20 +1300

    wip

--- day09.py
+++ day09.py
@@ -3,17 +3,18 @@
 from functools import reduce
 from itertools import cycle
 from pathlib import Path
+# from rich import print
 import math
 import re
 
 # https://adventofcode.com/2024/day/9
 
 INPUT_FILENAME = "input09.txt"
-INPUT_FILENAME = "sample09.txt"
+# INPUT_FILENAME = "sample09.txt"
 
 data = list(map(int, Path(INPUT_FILENAME).read_text().strip()))
 
-part1 = 0
+part1 = []
 part2 = 0
 
 head = 0
@@ -22,9 +23,7 @@ assert tail % 2 == 0
 tail_id = None
 tail_size = 0
 
-block_position = 0
-
-print(data)
+# print(data)
 
 while head <= tail:
     file_size = data
@@ -34,9 +33,7 @@ while head <= tail:
         # print("file:", data[head])
         for i in range(data[head]):
             file_id = head // 2
-            part1 += file_id * block_position
-            block_position += 1
-            print(file_id, end="")
+            part1.append(file_id)
     else:
         # this is blank space
         # print("blank:", data[head])
@@ -46,9 +43,8 @@ while head <= tail:
                 tail_size = data[tail]
                 tail -= 2
 
-            part1 += tail_id * block_position
-            block_position += 1
-            print(tail_id, end="")
+            part1.append(tail_id)
+            # print(tail_id, end="")
             tail_size -= 1
 
         pass
@@ -57,11 +53,11 @@ while head <= tail:
 
 
 while tail_size:
-    print(tail_id, end="")
-    part1 += tail_id * block_position
+    # print(tail_id, end="")
+    part1.append(tail_id)
     tail_size -= 1
 
-print("\n")
+# print("\n")
 
-print("part1:", part1)
+print("part1:", sum([i * v for i, v in enumerate(part1)]))
 # print("part2:", part2)
