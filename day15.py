3c2b5b1bdc8f7808d25a3c553e4cb603ed1bd0f0
Martyn Smith <martyn@dollyfish.net.nz>
2024-12-15 20:44:59 +1300

    Now with both parts

--- day15.py
+++ day15.py
@@ -1,73 +1,88 @@
 #!/usr/bin/env python
 
-from functools import reduce
 from rich import print
-from itertools import cycle
 from pathlib import Path
-import math
-import re
 
-from aoclib import build_grid, print_grid, Vector
+from aoclib import build_grid, Vector
 
 # https://adventofcode.com/2024/day/15
 
 INPUT_FILENAME = "input15.txt"
-INPUT_FILENAME = "sample15.txt"
+# INPUT_FILENAME = "sample15.txt"
 
-data = Path(INPUT_FILENAME).read_text().split('\n\n')
-# data = [line.strip() for line in Path(INPUT_FILENAME).read_text().splitlines()]
+warehouse_str, directions_str = Path(INPUT_FILENAME).read_text().split('\n\n')
 
-warehouse = build_grid(data[0].splitlines())
+p1_warehouse = build_grid(warehouse_str.splitlines())
+p2_warehouse = build_grid(
+    warehouse_str
+    .replace('#', '##')
+    .replace('O', '[]')
+    .replace('.', '..')
+    .replace('@', '@.')
+    .splitlines()
+)
 
-position = None
-for coord, item in warehouse.items():
-    if item == '@':
-        position = coord
-        # warehouse[coord] = '.'
-        break
+directions = [
+    {
+        'v': Vector(0, 1),
+        '^': Vector(0, -1),
+        '>': Vector(1, 0),
+        '<': Vector(-1, 0),
+    }[c] for c in directions_str.replace('\n', '')
+]
 
-direction_map = {
-    'v': Vector(0, 1),
-    '^': Vector(0, -1),
-    '>': Vector(1, 0),
-    '<': Vector(-1, 0),
-}
+p1_position = next(coord for coord, item in p1_warehouse.items() if item == '@')
+p2_position = next(coord for coord, item in p2_warehouse.items() if item == '@')
 
 
-# print_grid(warehouse)
 
-for direction_str in data[1].strip():
-    if direction_str == "\n":
-        continue
-    # print(direction_str)
-    d = direction_map[direction_str]
-
-    search_position = position + d
-    while warehouse[search_position] == 'O':
-        search_position += d
-
-    match warehouse[search_position]:
+def get_coords_to_move(position, direction, warehouse) -> set[Vector]:
+    match warehouse[position + direction]:
         case '.':
-            while search_position != position:
-                warehouse[search_position] = warehouse[search_position - d]
-                search_position -= d
-            warehouse[position] = '.'
-            position += d
+            return {position}
         case '#':
-            pass
-        case _:
-            print(f"{direction_str=}")
-            print(f"{position=}")
-            print(f"{search_position=}")
-            print_grid(warehouse)
-            raise NotImplementedError(f"Unexpected character: {warehouse[search_position]}")
+            return set()
+        case 'O':
+            # part 1 logic
+            moves = get_coords_to_move(position + direction, direction, warehouse)
+            if moves:
+                return {position} | moves
+            return set()
+        case '[' | ']':
+            # part 1 logic
+            m1 = get_coords_to_move(position + direction, direction, warehouse)
+            if direction.y:  # only for vertical movement do we need to account for the block width
+                m2 = get_coords_to_move(Vector(position.x + (1 if warehouse[position + direction] == '[' else -1), position.y) + direction, direction, warehouse)
+                if m1 and m2:
+                    return {position} | m1 | m2
+                else:
+                    return set()
+            elif m1:
+                return {position} | m1
+            else:
+                return set()
 
-part1 = 0
-for coord, item in warehouse.items():
-    if item == 'O':
-        part1 += 100 * coord.y + coord.x
+    raise NotImplementedError(f"{warehouse[position + direction]=}")
 
-print_grid(warehouse)
+def update_warehouse(position, direction, warehouse):
+    moves = get_coords_to_move(position, direction, warehouse)
+    if moves:
+        # Update the warehouse to be:
+        # existing warehouse + '.' for all moving tiles + the new position for all the moving tiles
+        return (
+            warehouse
+            | {v: '.' for v in moves}
+            | {v + direction: warehouse[v] for v in moves}
+        ), position + direction
+    return warehouse, position
 
-print("part1:", part1)
-# print("part2:", part2)
+for d in directions:
+    p1_warehouse, p1_position = update_warehouse(p1_position, d, p1_warehouse)
+    p2_warehouse, p2_position = update_warehouse(p2_position, d, p2_warehouse)
+
+def score(warehouse):
+    return sum(100 * v.y + v.x for v, item in warehouse.items() if item in {'O', '['})
+
+
+print("part1:", score(p1_warehouse))
+print("part2:", score(p2_warehouse))
